ab967f0f4a40e1284c67450a203f6f19481193d2
Михаил Грасс <miggra@mail.ru>
2023-07-10 14:53:34 +0300

    Add forecat template

---
+++ InterviewAspNetCore/Controllers/WeatherForecastController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace InterviewAspNetCore.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ InterviewAspNetCore/Services/WeatherService.cs
@@ -0,0 +1,22 @@
+namespace InterviewAspNetCore.Servioces;
+
+public class WeatherService
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly int forecastPeriodDay;
+
+    public IEnumerable<WeatherForecast> GetForecast()
+    {
+        return Enumerable.Range(1, forecastPeriodDay).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
