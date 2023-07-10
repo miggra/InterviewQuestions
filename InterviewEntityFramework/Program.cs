863e411c14acfd370c76ec28b94b05e28094e9b4
Михаил Грасс <miggra@mail.ru>
2023-07-10 14:14:10 +0300

    Add projection question

--- InterviewEntityFramework/Program.cs
+++ InterviewEntityFramework/Program.cs
@@ -7,6 +7,15 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Projection question
+        // foreach (var blog in context.Blogs)
+        // {
+        //     Console.WriteLine("Blog: " + blog.Url);
+        // }
 
+        // foreach (var blogName in context.Blogs.Select(b => b.Url))
+        // {
+        //     Console.WriteLine("Blog: " + blogName);
+        // }
     }
 }
