5b31e3617969bcdfc264f01b5509208e92306a86
Михаил Грасс <miggra@mail.ru>
2023-07-10 15:03:44 +0300

    sb q

--- InterviewWorkAround/Program.cs
+++ InterviewWorkAround/Program.cs
@@ -1,6 +1,25 @@
-﻿class Program
+﻿using System.Text;
+
+class Program
 {
-    static void Main(string[] args)
+    const int MAX_THREAD_COUNT = 42;
+    private static StringBuilder stringBuilder = new StringBuilder();
+
+    public static async Task Main()
     {
+        Task[] myTasks = new Task[MAX_THREAD_COUNT];
+        for (int i = 0; i < MAX_THREAD_COUNT; i++)
+        {
+            myTasks[i] = (MyTestMethod(i));
+        }
+        Task.WaitAll(myTasks);
+        Console.WriteLine(stringBuilder);
+    }
+
+    public static async Task MyTestMethod(int threadNum)
+    {
+        stringBuilder.AppendLine($"Я поток {threadNum} стартовал");
+        await Task.Delay(1000);
+        stringBuilder.AppendLine($"Я поток {threadNum} завершился");
     }
 }
