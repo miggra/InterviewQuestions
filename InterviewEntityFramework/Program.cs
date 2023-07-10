04ff646cec1257a10858d97e315874ef5ac51bcd
Михаил Грасс <miggra@mail.ru>
2023-07-10 13:48:54 +0300

    Add ef core blogging project

---
+++ InterviewEntityFramework/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+using InterviewEntityFramework;
+using InterviewEntityFramework.Entities;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
